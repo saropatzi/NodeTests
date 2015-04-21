fae329b578da44e07925a8205b3459bd632daf4a
David Carrus tech463@yoox.com <carrusd@MOBCTEK112.yoox.net>
2015-04-21 14:06:43 +0200

    cambio porta

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log("Hello World");
+  
   res.end('Hello World');
-}).listen(8080);
+}).listen(3270);
+
+console.log("Hello World");
